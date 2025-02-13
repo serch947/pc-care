e8a879752e2c23b8d7a4c3b1a85b71da12986d55
serch947 <serch947@gmail.com>
2025-02-13 00:09:08 -0500

    Index

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PC-Care</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <div class="menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a>
+                <ul class="dropdown">
+                    <li><a href="#">Laptops</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Acer</a></li>
+                            <li><a href="#">ASUS</a></li>
+                            <li><a href="#">Lenovo</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Desktops</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Dell</a></li>
+                            <li><a href="#">HP</a></li>
+                            <li><a href="#">Samsung</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="#">Services</a></li>
+</body>
+</html>
